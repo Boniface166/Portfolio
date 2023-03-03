7bb58bbdb30e32a203f5827f8fa7041172b811bd
Boniface Ngetich <116899434+Boniface166@users.noreply.github.com>
2023-03-03 13:48:00 +0300

    added typing effect

---
+++ index.js
@@ -0,0 +1,27 @@
+// jQuery
+$(document).ready(function() {
+    var text = "Hello, My name is"; // Text to be typed
+    var speed = 50; // Typing speed in milliseconds
+    
+    function typeWriter(text, i, fnCallback) {
+      if (i < text.length) {
+        $('#typing').html(text.substring(0, i+1));
+        setTimeout(function() {
+          typeWriter(text, i+1, fnCallback)
+        }, speed);
+      } else if (typeof fnCallback == 'function') {
+        setTimeout(fnCallback, 700);
+      }
+    }
+    
+    function startTyping() {
+      if (typeof text === 'string') {
+        typeWriter(text, 0, function() {
+          startTyping();
+        });
+      }
+    }
+    
+    startTyping();
+  });
+  
